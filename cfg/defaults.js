215d55b780d4b699bdd3ec41ec1d399f3e73dafb
guomiao01 <guomiao01@baidu.com>
2016-07-30 16:47:04 +0800

    add style

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -12,7 +12,7 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
+        loader: 'style-loader!css-loader!postcss-loader'
       },
       {
         test: /\.sass/,
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,26 +3,35 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
- 
 // get images data
-//let imageDatas = require('../data/imageData.json');
+var imageDatas = require('../data/imageData.json');
 
 // change the images name into images url
-/*imageDatas = (function genImageURL(imageDatasArr) {
-    for (var i = 0, len = imageDatasArr.length; i < len; i++) {
-        var singleImageData = imageDatasArr[i];
-        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+function genImageURL(imageDatasArr) {
+    try {
+        for (var i = 0, len = imageDatasArr.length; i < len; i++) {
+            var singleImageData = imageDatasArr[i];
+            if (singleImageData) {
+                singleImageData.imageURL = require('../images/' + singleImageData.filename);
+            }
+        }
     }
-})(imageDatas);
-*/
+    catch (e) {
+        alert(e.message);
+    }
+}
+genImageURL(imageDatas);
+
+
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Are you Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <section className="stage">
+            <section className="img-sec">
+            </section>
+            <nav className="controller-nav">
+            </nav>
+        </section>
     );
   }
 }
